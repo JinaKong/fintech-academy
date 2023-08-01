794a3479fc83a2fd53984dfe988595e64c660355
jina kong <kongjn226@gmail.com>
2023-08-01 22:59:27 +0900

    Add Component using List

---
+++ fintech/src/components/ListComponent.js
@@ -0,0 +1,23 @@
+//rafce 단축어 사용함
+import React, { useState } from 'react'
+import Welcome from './Welcome';
+
+const ListComponent = () => {
+    const [users, setUsers] = useState( [
+        {username: "홍길동", age:12, major:"경영학"},
+        {username: "고길동", age:22, major:"경제학"},
+        {username: "김길동", age:32, major:"사회학"},
+    ]);
+
+  return (
+    <div>
+        {users.map((user) => {
+            return (
+            <Welcome userName={user.username} age={user.age}></Welcome>
+            );
+        })}
+    </div>
+  );
+};
+
+export default ListComponent
